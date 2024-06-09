1adc0867166bc35dd960dda4e709446854d3c286
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-09 15:21:50 -0700

    notifier

---
+++ notifier.js
@@ -0,0 +1,107 @@
+(function(Scratch) {
+    'use strict';
+  
+    if (!Scratch.extensions.unsandboxed) {
+      throw new Error('This extension must run unsandboxed');
+    }
+  
+    class HelloWorld {
+      getInfo() {
+        return {
+          id: 'notification',
+          name: 'Notify',
+          blocks: [
+            {
+              opcode:'sendnotification',
+              blockType: Scratch.BlockType.COMMAND,
+              text: 'Notify [TITLE] [BODY]',
+              arguments: {
+                TITLE: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: 'Title'
+                },
+                BODY: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: 'Body'
+                }
+              }
+            },
+            {
+                opcode:'sendtaggednotif',
+                blockType: Scratch.BlockType.COMMAND,
+                text: 'Dynamic Notify [TITLE] [BODY] [TAG]',
+                arguments: {
+                  TITLE: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'Title'
+                  },
+                  BODY: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'Body'
+                  },
+                  TAG: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'Tag'
+                  }
+                }
+              },
+              {
+                opcode:'sendimgnotif',
+                blockType: Scratch.BlockType.COMMAND,
+                text: 'Dynamic Notify [TITLE] [BODY] [IMG]',
+                arguments: {
+                  TITLE: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'Title'
+                  },
+                  BODY: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'Body'
+                  },
+                  IMG: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'https://imageserver.xyz/path/to/image.png'
+                  }
+                }
+              }
+          ]
+        };
+      }
+      sendnotification(args) {
+        Notification.requestPermission().then(perm => {
+            if (perm === "granted") {
+                new Notification(args.TITLE, {
+                    body: args.BODY,
+                })
+            } else {
+                throw new error('Error: couldnt get notification permission from browser')
+            }
+        })
+      }
+      sendtaggednotif(args) {
+        Notification.requestPermission().then(perm => {
+            if (perm === "granted") {
+                new Notification(args.TITLE, {
+                    body: args.BODY,
+                    tag: args.TAG,
+                })
+            } else {
+                throw new error('Error: couldnt get notification permission from browser')
+            }
+        })
+      }
+      sendimgnotif(args) {
+        Notification.requestPermission().then(perm => {
+            if (perm === "granted") {
+                new Notification(args.TITLE, {
+                    body: args.BODY,
+                    icon: args.IMG,
+                })
+            } else {
+                throw new error('Error: couldnt get notification permission from browser')
+            }
+        })
+      }
+    }
+    Scratch.extensions.register(new HelloWorld());
+  })(Scratch);
