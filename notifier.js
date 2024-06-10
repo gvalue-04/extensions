36630b95cae2d57f08fa07740a2fb0396d66ac4b
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-09 21:53:55 -0400

    fix

--- notifier.js
+++ notifier.js
@@ -1,121 +1,146 @@
-(function(Scratch) {
-    'use strict';
-  
-    if (!Scratch.extensions.unsandboxed) {
-      throw new Error('This extension must run unsandboxed');
+(function (Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This extension must run unsandboxed');
+  }
+  let denied = false;
+
+  const askForNotificationPermission = async () => {
+    try {
+
+      const allowedByBrowser = await Notification.requestPermission();
+      if (!allowedByBrowser) {
+        throw new Error("Denied by browser");
+      }
+      denied = false;
+      return true;
+    } catch (e) {
+      denied = true;
+      console.warn("Could not request notification permissions", e);
+      return false;
     }
+  };
+
+  const isAndroid = () => navigator.userAgent.includes("Android");
+
+  const getServiceWorkerRegistration = () => {
+    if (!("serviceWorker" in navigator)) return null;
+    // This is only needed on Android
+    if (!isAndroid()) return null;
+    return navigator.serviceWorker.getRegistration();
+  };
   
-    class HelloWorld {
-      getInfo() {
-        return {
-          id: 'notification',
-          name: 'Notify',
-          blocks: [
-            {
-              opcode:'sendnotification',
-              blockType: Scratch.BlockType.COMMAND,
-              text: 'Notify [TITLE] [BODY]',
-              arguments: {
-                TITLE: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: 'Title'
-                },
-                BODY: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: 'Body'
-                }
+  class HelloWorld {
+    getInfo() {
+      return {
+        id: 'notification',
+        name: 'Notify',
+        blocks: [
+          {
+            opcode: 'sendnotification',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Notify [TITLE] [BODY]',
+            arguments: {
+              TITLE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Title'
+              },
+              BODY: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Body'
               }
-            },
-            {
-                opcode:'sendtaggednotif',
-                blockType: Scratch.BlockType.COMMAND,
-                text: 'Dynamic Notify [TITLE] [BODY] [TAG]',
-                arguments: {
-                  TITLE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'Title'
-                  },
-                  BODY: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'Body'
-                  },
-                  TAG: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'Tag'
-                  }
-                }
+            }
+          },
+          {
+            opcode: 'sendtaggednotif',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Dynamic Notify [TITLE] [BODY] [TAG]',
+            arguments: {
+              TITLE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Title'
               },
-              {
-                opcode:'sendimgnotif',
-                blockType: Scratch.BlockType.COMMAND,
-                text: 'Image Notify [TITLE] [BODY] [IMG]',
-                arguments: {
-                  TITLE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'Title'
-                  },
-                  BODY: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'Body'
-                  },
-                  IMG: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'https://extensions.turbowarp.org/dango.png'
-                  }
-                }
+              BODY: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Body'
               },
-                {
-                opcode:'haspermissionnotif',
-                blockType: Scratch.BlockType.BOOLEAN,
-                text: 'has notification permission?',
-                }
-          ]
-        };
-      }
-      sendnotification(args) {
-        Notification.requestPermission().then(perm => {
-            if (perm === "granted") {
-                new Notification(args.TITLE, {
-                    body: args.BODY,
-                })
-            } else {
-                throw new error('Error: couldnt get notification permission from browser')
+              TAG: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Tag'
+              }
             }
-        })
-      }
-      sendtaggednotif(args) {
-        Notification.requestPermission().then(perm => {
-            if (perm === "granted") {
-                new Notification(args.TITLE, {
-                    body: args.BODY,
-                    tag: args.TAG,
-                })
-            } else {
-                throw new error('Error: couldnt get notification permission from browser')
+          },
+          {
+            opcode: 'sendimgnotif',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Image Notify [TITLE] [BODY] [IMG]',
+            arguments: {
+              TITLE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Title'
+              },
+              BODY: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Body'
+              },
+              IMG: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/dango.png'
+              }
             }
-        })
-      }
-      sendimgnotif(args) {
-        Notification.requestPermission().then(perm => {
-            if (perm === "granted") {
-                new Notification(args.TITLE, {
-                    body: args.BODY,
-                    icon: args.IMG,
-                })
-            } else {
-                throw new error('Error: couldnt get notification permission from browser')
-            }
-        })
-      }
-        haspermissionnotif() {
-        Notification.requestPermission().then(perm => {
-            if (perm === "granted") {
-                return true;
-            } else {
-                return false;
-            }
-        })
+          },
+          {
+            opcode: 'haspermissionnotif',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'has notification permission?',
+          }
+        ]
+      };
+    }
+    sendnotification(args) {
+      Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+          new Notification(args.TITLE, {
+            body: args.BODY,
+          })
+        } else {
+          throw new error('Error: couldnt get notification permission from browser')
+        }
+      })
+    }
+    sendtaggednotif(args) {
+      Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+          new Notification(args.TITLE, {
+            body: args.BODY,
+            tag: args.TAG,
+          })
+        } else {
+          throw new error('Error: couldnt get notification permission from browser')
+        }
+      })
+    }
+    sendimgnotif(args) {
+      Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+          new Notification(args.TITLE, {
+            body: args.BODY,
+            icon: args.IMG,
+          })
+        } else {
+          throw new error('Error: couldnt get notification permission from browser')
+        }
+      })
+    }
+    haspermissionnotif() {
+      askForNotificationPermission();
+      if (denied) {
+        return false;
+      } else {
+        return true;
       }
     }
-    Scratch.extensions.register(new HelloWorld());
-  })(Scratch);
+  }
+  Scratch.extensions.register(new HelloWorld());
+})(Scratch);
